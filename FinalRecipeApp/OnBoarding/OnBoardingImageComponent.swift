97c7fb1904c38a76b2c15fa4208f6bbe2a33e665
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-01-19 00:55:33 +0400

    first commit

---
+++ FinalRecipeApp/OnBoarding/OnBoardingImageComponent.swift
@@ -0,0 +1,8 @@
+//
+//  OnBoardingImageComponent.swift
+//  FinalRecipeApp
+//
+//  Created by Mariam Joglidze on 19.01.24.
+//
+
+import Foundation
---
+++ FinalRecipeApp/OnBoarding/OnBoardingPage.swift
@@ -0,0 +1,29 @@
+//
+//  OnBoardingPage.swift
+//  FinalRecipeApp
+//
+//  Created by Mariam Joglidze on 18.01.24.
+//
+
+import UIKit
+
+class OnBoardingPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
