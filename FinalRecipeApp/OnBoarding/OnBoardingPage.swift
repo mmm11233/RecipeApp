05f314748f87e40308bc2ade8e7afc505e4c0ec2
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-01-19 02:16:36 +0400

    add appTitle label and refactor code

--- FinalRecipeApp/OnBoarding/OnBoardingPage.swift
+++ FinalRecipeApp/OnBoarding/OnBoardingPage.swift
@@ -8,18 +8,34 @@
 import UIKit
 import SwiftUI
 
-class OnBoardingPage: UIViewController {
+final class OnBoardingPage: UIViewController {
+    
     //MARK: properties
-    let mainImage: UIImageView = {
+    private let mainImage: UIImageView = {
         return createImageView(with: "image 8")
     }()
     
-    let middleImage: UIImageView = {
+    private let middleImage: UIImageView = {
         return createImageView(with: "Ellipse 10")
     }()
     
-    let ellipseImage: UIImageView = {
+    private let ellipseImage: UIImageView = {
         return createImageView(with: "Ellipse 8")
+    }()
+    
+    private let appTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Savory & Sweet"
+        label.textColor = .white
+        label.numberOfLines = 2
+        label.font = label.font.withSize(36)
+        label.layer.shadowColor = UIColor.white.cgColor
+        label.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        label.layer.shadowOpacity = 0.5
+        label.layer.shadowRadius = 2.0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
     }()
     
     //MARK: lifeCycle
@@ -31,43 +47,37 @@ class OnBoardingPage: UIViewController {
     
     //MARK: methods
     private func setupUI() {
-        mainImage.contentMode = .scaleAspectFill
-        view.addSubview(mainImage)
+        [mainImage, ellipseImage, middleImage].forEach {
+            $0.contentMode = .scaleAspectFill
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+
+        view.addSubview(appTitle)
         
-        mainImage.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             mainImage.topAnchor.constraint(equalTo: view.topAnchor),
             mainImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mainImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            mainImage.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
-        ellipseImage.contentMode = .scaleAspectFill
-        view.addSubview(ellipseImage)
-        ellipseImage.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            mainImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
             ellipseImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             ellipseImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             ellipseImage.widthAnchor.constraint(equalToConstant: 225),
-            ellipseImage.heightAnchor.constraint(equalToConstant: 225)
-        ])
-        
-        middleImage.contentMode = .scaleAspectFill
-        view.addSubview(middleImage)
-        middleImage.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            ellipseImage.heightAnchor.constraint(equalToConstant: 225),
+            
             middleImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             middleImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             middleImage.widthAnchor.constraint(equalToConstant: 210),
-            middleImage.heightAnchor.constraint(equalToConstant: 210)
+            middleImage.heightAnchor.constraint(equalToConstant: 210),
+            
+            appTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appTitle.bottomAnchor.constraint(equalTo: ellipseImage.topAnchor, constant: -45)
         ])
-        
     }
 }
 
 #Preview {
     let vc = OnBoardingPage()
-    
     return vc
 }
-
-
