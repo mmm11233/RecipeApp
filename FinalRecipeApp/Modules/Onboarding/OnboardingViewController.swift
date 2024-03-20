c49e5921aba6c05b04d7613c0c71cff0b728f8ff
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-03-20 23:29:04 +0400

    setup Onboarding collectionView

--- FinalRecipeApp/Modules/Onboarding/OnboardingViewController.swift
+++ FinalRecipeApp/Modules/Onboarding/OnboardingViewController.swift
@@ -7,28 +7,49 @@
 
 import UIKit
 
-class OnboardingCollectionView: UIViewController {
+class OnboardingViewController: UIViewController {
     
     // MARK: - Properties
     private let collectionView: UICollectionView = {
-        let collectionView = UICollectionView()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         
         return collectionView
     }()
+
+    var viewModel:  OnboardingViewModel
     
-    var viewModel: OnboardingViewModel!
+    init(viewModel: OnboardingViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel.setup()
         setupView()
+        setupCollectionView()
         setupConstraints()
     }
     
     // MARK: - Configure
     private func setupView() {
         view.addSubview(collectionView)
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(OnboardingCollectionViewCell.self, forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .clear
     }
     
     private func setupConstraints() {
@@ -39,19 +60,23 @@ class OnboardingCollectionView: UIViewController {
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
+
 }
     
     //MARK: - Extensions
     
-extension OnboardingCollectionView: UICollectionViewDataSource, UICollectionViewDelegate {
+extension OnboardingViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        viewModel.items.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCollectionViewCell", for: indexPath)
+       guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCollectionViewCell", for: indexPath) as? OnboardingCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: viewModel.items[indexPath.row])
+        return cell
         
     }
-    
 }
 
